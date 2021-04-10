2b5aeff8386f71d9d0a406a7603a213ebad54b27
hmorfeld <hmorfeld91@gmail.com>
2021-04-10 14:23:03 -0500

    added shouldShowWarning

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -13,10 +13,16 @@ export class Satellite {
     }
 
     shouldShowWarning (){
-        if (this.type.toLowerCase === "Space Debris".toLowerCase){
+        if (this.type.toLowerCase === "Space Debris".toLowerCase || this.type === "Space Debris"){
             return true
         } else {
             return false
         }
     }
 }
+// Questions: 
+// Local host not working
+//"app-orbit-list" not a known element; how do I verify that it is part of the module?
+//Can't bind to 'satellites' since it isn't a known property of 'app-orbit-list'. ^ same as above.
+//not really understanding how to use shouldShowWaring  to add the warning CSS class to the <td> containing the satellite's type
+// An argument for "name" was not provided in new satellite? But it was? 
